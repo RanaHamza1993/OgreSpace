8c8f17c9c04189335e8147820f7e1d3af9adf902
Hafiz Hamza <hafiz.hamza@brainplow.com>
2019-07-23 18:03:48 +0500

    push

--- app/src/main/java/com/brainplow/ogrespace/adapters/ServiceAdapter.kt
+++ app/src/main/java/com/brainplow/ogrespace/adapters/ServiceAdapter.kt
@@ -18,7 +18,8 @@ class ServiceAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     var mContext: Context? = null
     var sServiceArray: ArrayList<StateModel>? = null
-
+    var servicesStringArray= ArrayList<String>()
+    val sb_user = StringBuilder()
 
     constructor(mContext: Context, sServiceArray: ArrayList<StateModel>) : this() {
         this.mContext = mContext
@@ -43,40 +44,10 @@ class ServiceAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        var holder: ViewHolder = holder as ViewHolder
-        var user: String? = null
-        var sb_user = StringBuilder()
-        var myList = JSONArray()
-        var user_pref: String? = null
+        val holder: ViewHolder = holder as ViewHolder
 
+        holder.setData(position)
 
-        holder.service_title?.setText(sServiceArray?.get(position)!!.state)
-
-        Glide.with(mContext!!)
-            .load(sServiceArray?.get(position)?.icon_image)
-            .into(holder.service_image!!);
-
-        holder?.main_layout?.setOnClickListener(View.OnClickListener {
-            if (sServiceArray?.get(position)!!.check_value == false) {
-                sServiceArray?.get(position)!!.check_value = true
-            } else {
-                sServiceArray?.get(position)!!.check_value = false
-            }
-
-            for (i in 0 until sServiceArray!!.size)
-                if (sServiceArray?.get(i)?.check_value == true) {
-
-                    user = sServiceArray!!.get(i).state
-                    user_pref = sb_user.append(user + " ").toString()
-
-                    myList.put(user!!)
-                }
-
-
-            AddListingFragment?.et_space_services?.setText(user_pref)
-
-            notifyDataSetChanged()
-        })
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -85,9 +56,53 @@ class ServiceAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         var main_layout: LinearLayout? = null
 
         init {
-            main_layout = view?.findViewById(R.id.main_layout)
-            service_image = view?.findViewById(R.id.service_title)
-            service_title = view?.findViewById(R.id.service_txt)
+            main_layout = view.findViewById(R.id.main_layout)
+            service_image = view.findViewById(R.id.service_title)
+            service_title = view.findViewById(R.id.service_txt)
+        }
+
+        fun setData(position: Int){
+
+            service_title?.setText(sServiceArray?.get(position)!!.state)
+
+            Glide.with(mContext!!)
+                .load(sServiceArray?.get(position)?.icon_image)
+                .into(service_image!!);
+
+            itemView.setOnClickListener(View.OnClickListener {
+                if (sServiceArray?.get(position)!!.check_value == false) {
+                    sServiceArray?.get(position)!!.check_value = true
+                    sb_user.append(sServiceArray!!.get(position).state!!+" ")
+                    //     servicesStringArray.add(sServiceArray!!.get(position).state!!)
+                } else {
+                    sServiceArray?.get(position)!!.check_value = false
+                    val index=sb_user.indexOf(sServiceArray!![position].state!!)
+                    if(index!=-1){
+                        sb_user.delete(index,index+sServiceArray!![position].state!!.length)
+                    }
+                    servicesStringArray.remove(sServiceArray!![position].state!!)
+                }
+
+//            for(state in servicesStringArray){
+//                user_pref = sb_user.append(state + " ").toString()
+//
+//            }
+
+//            for (i in 0 until sServiceArray!!.size)
+//                if (sServiceArray?.get(i)?.check_value == true) {
+//
+//                    user = sServiceArray!!.get(i).state
+//                    user_pref = sb_user.append(user + " ").toString()
+//
+//                    myList.put(user!!)
+//                }
+
+
+                // AddListingFragment.et_space_services?.setText(user_pref)
+                AddListingFragment.et_space_services?.setText(sb_user.toString())
+
+                notifyDataSetChanged()
+            })
         }
 
     }
